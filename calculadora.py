3a27acc4f6536413f9f5f9ae225ea615ba040886
tamoil <jaracayo7@gmail.com>
2026-01-30 18:53:28 -0500

    feat: crear clase Calculator base con constructor e historial

--- calculadora.py
+++ calculadora.py
@@ -8,3 +8,13 @@ Desarrollada como parte del plan de desarrollo de 2 meses
 
 import math
 import sys
+
+
+class Calculator:
+    """Clase base de la calculadora científica"""
+    
+    def __init__(self):
+        """Inicializa la calculadora"""
+        self.resultado = 0
+        self.historial = []
+
