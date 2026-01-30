a52aca85613cf6eabf33296d24c9cf62a5bba8a0
tamoil <jaracayo7@gmail.com>
2026-01-30 18:52:48 -0500

    feat: agregar header y documentación inicial del proyecto

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+# Calculadora Científica Avanzada
+# Proyecto de Desarrollo - Fase 1: Fundación
+
+"""
+Calculadora científica con funciones básicas y avanzadas
+Desarrollada como parte del plan de desarrollo de 2 meses
+"""
